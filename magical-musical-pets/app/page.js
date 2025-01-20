60dddd944899552b82f2670d8e9a239dc871d03a
weisop <120297357+weisop@users.noreply.github.com>
2025-01-20 14:21:47 -0800

    test push

--- magical-musical-pets/app/page.js
+++ magical-musical-pets/app/page.js
@@ -1,95 +1,58 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import "./globals.css";
+import "./page.css";
+import { useState } from "react";
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isDisplayPopup, setDisplayPopup] = useState(false);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const openPopup = () => {
+    setDisplayPopup(true);
+  };
+
+  const closePopup = () => {
+    setDisplayPopup(false);
+  };
+
+  const renderPopup = () => {
+    if (!isDisplayPopup) {
+      return null;
+    }
+    return (
+      <div id="popup-modal" className="modal">
+        <div className="modal-content">
+        <span className="close-btn" onClick={closePopup}>&times;</span>
+          <h2 style={{marginTop: "0"}}>Welcome to Magical Musical Pets!</h2>
+          <p>Are you ready to begin your adventure?</p>
+          <button onClick={closePopup}>OK</button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Image src="/mmp.png" alt="Logo" id="logo" width={500} height={500} />
+      <nav id="homepage-nav">
+        <Link href="homepage">Home</Link>
+        <Link href="calendar">Calendar</Link>
+        <Link href="pet-customization">Pet Customization</Link>
+      </nav>
+      <main>
+        {renderPopup()}
+        <div id="floating-container">
+          <Image src="/RightFloatBg.png" alt="" id="container-image" width={540} height={540} />
+          <button id="container-button" onClick={openPopup}>Get Started!</button>
+        </div>
+        <div id="gif-wrapper">
+          <div id="floating-container-left"></div>
+          <Image src="/Magical Musical pets.gif" id="title" alt="Magical Musical Pets GIF"
+            width={500} height={500} />
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
